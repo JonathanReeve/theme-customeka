49e6d61fcbeabca4ce438a9cca6dd9b672f41c97
Jonathan Reeve <jon.reeve@gmail.com>
2012-08-27 15:40:54 -0400

    fixed paragraph tags in footer

--- common/footer.php
+++ common/footer.php
@@ -1,7 +1,7 @@
 <div id="footer">
 	<?php echo get_theme_option('Footer Text'); ?>
 	<?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = settings('copyright')): ?>
-                    <p><?php echo $copyright; ?></p>
+                    <?php echo $copyright; ?>
 	<?php endif; ?>
 	<a href="http://omeka.org" id="omeka-logo">Proudly Powered by Omeka</a>
 	<?php plugin_footer(); ?>
