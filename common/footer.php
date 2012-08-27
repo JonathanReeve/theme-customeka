14a5d7f46ef0c1e4e469645aa14cc1f1287d3bf5
Jonathan Reeve <jon.reeve@gmail.com>
2012-08-27 15:39:24 -0400

    fixed footer text customization, fixes #7

--- common/footer.php
+++ common/footer.php
@@ -1,9 +1,12 @@
 <div id="footer">
+	<?php echo get_theme_option('Footer Text'); ?>
+	<?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = settings('copyright')): ?>
+                    <p><?php echo $copyright; ?></p>
+	<?php endif; ?>
 	<a href="http://omeka.org" id="omeka-logo">Proudly Powered by Omeka</a>
+	<?php plugin_footer(); ?>
 </div>
-
 </div>
-
 </div>
 </body>
 </html>
