818b1d43b4f19b7e082808f86c89f3ad73086df6
Jonathan Reeve <jon.reeve@gmail.com>
2013-08-07 08:48:28 -0500

    updated site title function syntax

--- common/header.php
+++ common/header.php
@@ -2,7 +2,7 @@
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php echo get_html_lang(); ?>" class="<?php echo (get_theme_option('Style Sheet') ? get_theme_option('Style Sheet') : 'vertical'); ?>"> 
 <head>
-<title><?php echo settings('site_title'); ?></title>
+<title><?php echo option('site_title'); ?></title>
 <!-- Meta -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<!--Stylesheets-->
