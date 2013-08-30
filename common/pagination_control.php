aa6fdddfeadd2b1b257450ca026a554238f382b6
Jonathan Reeve <jon.reeve@gmail.com>
2013-08-30 18:46:21 -0500

    styled pagination, added a few experimental effects

--- common/pagination_control.php
+++ common/pagination_control.php
@@ -4,14 +4,14 @@
     <?php if ($this->first != $this->current): ?>
         <!-- First page link --> 
         <li>
-            <a href="<?php echo html_escape($this->url(array('page' => $this->first), null, $_GET)); ?>"><?php echo __('First'); ?></a>
+            <a href="<?php echo html_escape($this->url(array('page' => $this->first), null, $_GET)); ?>"><?php echo __('<<'); ?></a>
         </li>
     <?php endif; ?>
 
     <!-- Previous page link --> 
     <?php if (isset($this->previous)): ?>
     <li class="pagination_previous">
-    <a href="<?php echo html_escape($this->url(array('page' => $this->previous), null, $_GET)); ?>"><?php echo __('Prev'); ?></a>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->previous), null, $_GET)); ?>"><?php echo __('<'); ?></a>
     </li>
     <?php endif; ?>
     
@@ -27,14 +27,14 @@
     <?php if (isset($this->next)): ?> 
     <!-- Next page link -->
     <li class="pagination_next">
-    <a href="<?php echo html_escape($this->url(array('page' => $this->next), null, $_GET)); ?>"><?php echo __('Next'); ?></a>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->next), null, $_GET)); ?>"><?php echo __('>'); ?></a>
     </li>
     <?php endif; ?>
 
     <?php if ($this->last != $this->current): ?>
     <!-- Last page link --> 
     <li class="pagination_last">
-    <a href="<?php echo html_escape($this->url(array('page' => $this->last), null, $_GET)); ?>"><?php echo __('Last'); ?></a>
+    <a href="<?php echo html_escape($this->url(array('page' => $this->last), null, $_GET)); ?>"><?php echo __('>>'); ?></a>
     </li>
     <?php endif; ?>
 
