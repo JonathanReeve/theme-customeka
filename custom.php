93728f59f69d2cea371c182c903ac2b9cbeaf098
Jonathan Reeve <jon.reeve@gmail.com>
2014-02-23 20:01:59 -0600

    import custom.php

---
+++ custom.php
@@ -0,0 +1,45 @@
+<?php
+
+/* overrides queue_css function to work with custom css function */
+function elementaire_queue_css($file, $media = 'all', $conditional = false, $dir = 'css')
+{
+    if (is_array($file)) {
+        foreach($file as $singleFile) {
+            queue_css($singleFile, $media, $conditional, $dir);
+        }
+        return;
+    }
+    __v()->headLink()->appendStylesheet(elementaire_css($file, $dir), $media, $conditional);
+}
+/* overrides css function so that it will work with css files with the .php extension */
+
+function elementaire_css($file, $dir = 'css')
+{
+    return src($file, $dir);
+}
+
+/* adapted from Griddy theme */
+function elementaire_custom_background()
+{
+	if(function_exists('get_theme_option') && $bgimage = get_theme_option('Background Image')) {
+		$storage = Zend_Registry::get('storage');
+        	$bgimage_url = $storage->getUri($storage->getPathByType($bgimage, 'theme_uploads'));
+	        $html = $bgimage_url;
+	} else { 
+		$html = ""; 
+	} 
+        echo $html;
+}
+
+function elementaire_custom_header_background()
+{
+    if(function_exists('get_theme_option') && $headerBg = get_theme_option('Header Background')) {
+        $storage = Zend_Registry::get('storage');
+        $headerBg = $storage->getUri($storage->getPathByType($headerBg, 'theme_uploads'));
+	echo $headerBg;
+    }
+}
+
+
+?>
+
